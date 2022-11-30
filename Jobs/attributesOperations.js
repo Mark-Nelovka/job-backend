a3e0ef8d2a24e475830bf82258659a8ba0097d46
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2022-11-30 14:53:30 +0200

    fix atrOperations

--- Jobs/attributesOperations.js
+++ Jobs/attributesOperations.js
@@ -7,18 +7,18 @@ async function atrributesOperations(action, id, save, rating) {
   const jobs = await getJobs();
   switch (action) {
     case "save":
-      changeItem = jobs.map((it) =>
+      const changeSave = jobs.map((it) =>
         it.id === id ? { ...it, save: !save } : it
       );
-      await fs.writeFile(filePath, JSON.stringify(changeItem));
-      return changeItem;
+      await fs.writeFile(filePath, JSON.stringify(changeSave));
+      return changeSave;
 
     case "rating":
-      changeItem = jobs.map((it) =>
+      const changeRating = jobs.map((it) =>
         it.id === id ? { ...it, rating: rating } : it
       );
-      await fs.writeFile(filePath, JSON.stringify(changeItem));
-      return changeItem;
+      await fs.writeFile(filePath, JSON.stringify(changeRating));
+      return changeRating;
 
     default:
       return changeItem;
